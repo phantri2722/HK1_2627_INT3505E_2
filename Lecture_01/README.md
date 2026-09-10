a84210aca05226e8e8eb377a558f5492ca8d3f9e
Phan Trí <phantranquangtri1990@gmail.com>
2026-09-10 18:20:25 +0700

    Problem 2

---
+++ Lecture_01/README.md
@@ -0,0 +1,7 @@
+# Lecture 01: Introduction about API and Web Services
+
+## Problem 01
+![alt text](Problem_01.png)
+
+## Problem 02
+![alt text](Problem_02.png)
--- Lecture_01/app.py
+++ Lecture_01/app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return {"message": "Hello, API!"}
-
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
